e575ddc1897c3ff8b5da5fa7a6337f312f8cfebb
FaruqKhan11148 <faruqkhanshired111@gmail.com>
2026-01-18 14:58:58 +0530

    updations

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -1,16 +1,43 @@
 const express = require('express');
 const router = express.Router();
-const productModel = require('../models/productModel');
+const productService = require('../services/productService');
+const { protect } = require('../middlewares/authMiddleware');
 
 // login page
 router.get('/login', (req, res) => {
   res.render('pages/login'); // views/auth/login.ejs
 });
 
-router.get("/signup", (req, res)=>{
-  res.render("pages/signup");
-})
+router.get('/signup', (req, res) => {
+  res.render('pages/signup');
+});
 
+// get all products
+router.get('/products', protect, (req, res) => {
+  productService.fetchAllProducts((err, products) => {
+    if (err) {
+      return res.render('pages/error', {
+        message: 'Failed to load products',
+      });
+    }
 
+    res.render('pages/products', {
+      products,
+    });
+  });
+});
+
+// product details
+router.get('/products/:id', protect, (req, res) => {
+  productService.getProduct(req.params.id, (err, result) => {
+    if (err || !result.length) {
+      return res.render('pages/error', { message: 'Product not found' });
+    }
+
+    res.render('pages/productDetails', {
+      product: result[0],
+    });
+  });
+});
 
 module.exports = router;
