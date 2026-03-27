c1dab15e23ddc25be2870a61c6e338e1e69f40b5
Faruq Khan <150416913+FaruqKhan11148@users.noreply.github.com>
2026-03-27 16:42:53 +0530

    updations

--- controllers/addressController.js
+++ controllers/addressController.js
@@ -33,6 +33,7 @@ const editAddressPage = (req, res) => {
   );
 };
 
+// ACTION: CREATE ADDRESS
 const createAddress = (req, res) => {
   addressService.addNewAddress(req.user._id, req.body, (err) => {
     if (err) return res.status(500).send('DB Error');
